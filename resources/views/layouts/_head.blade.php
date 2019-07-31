88bd0e0ce45169047c171ea5c968e95b0abf93e5
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-07-31 14:29:10 -0400

    Update _head.blade.php

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -14,11 +14,16 @@
             'csrfToken' => csrf_token(),
         ]); ?>
     </script>
+    
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144975993-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-    <link rel="apple-touch-icon" href="{{ URL::asset('/images/apple-touch-icon-57x57.png') }}" />
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ URL::asset('/images/apple-touch-icon-76x76.png') }}" />
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ URL::asset('/images/apple-touch-icon-120x120.png') }}" />
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ URL::asset('/images/apple-touch-icon-152x152.png') }}" />
+    gtag('config', 'UA-144975993-1');
+    </script>
 
-    <link rel="shortcut icon" href="/favicon.ico?v1">
+
+    <link rel="shortcut icon" href="/favicon.ico">
 </head>
