fde7b02e5b17960f50277d28e7e6292873d1075e
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-07-31 12:48:54 -0400

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,1 @@
 
-<?php
-Route::group(['prefix' => 'v1'], function () {
-    Route::post('fullsize', 'v1\ScreenshotController@store')->middleware('auth:api');
-});
-Route::group(['prefix' => 'v2'], function () {
-    Route::post('screenshot', 'v2\ScreenshotController@store')->middleware('auth:api');
-});
