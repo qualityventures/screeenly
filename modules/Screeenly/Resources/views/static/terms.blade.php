e4b698c48d4ba5e1f27c48c6f3dab3cdca80da6f
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-07-31 15:11:27 -0400

    Update terms.blade.php

--- modules/Screeenly/Resources/views/static/terms.blade.php
+++ modules/Screeenly/Resources/views/static/terms.blade.php
@@ -14,7 +14,7 @@
             <p style="text-align: left;>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
             <p style="text-align: left;>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service.</p>
 
-            <h4">Accounts</h4>
+            <h4>Accounts</h4>
 
             <p style="text-align: left;>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
             <p style="text-align: left;>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</p>
