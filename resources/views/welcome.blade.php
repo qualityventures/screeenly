51ea524c6fd196d92adcc85ec785a5555dbf0110
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-07-31 14:43:15 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,12 +14,12 @@
 
         <div class="row">
             <div class="col-sm-4">
-                <h3>Simple</h3>
+                <h3>Easy as 1, 2, 3</h3>
                 <p>
-                    Just send your API key and a URL to our API and we create a fresh screenshot for you.
+                    Send a request to our API using your API key and a URL and will generate a fresh screenshot.
                 </p>
                 <p>
-                    Screenshots come as a file path or as a base64 encoded string. <br>Keep in mind that screenshots are deleted from our server after 1 hour.</br>
+                    Screenshots are generated as a file path or as a base64 encoded string. <b>Keep in mind that screenshots are deleted from our server after 1 hour.</b>
                 </p>
             </div>
         </div>
