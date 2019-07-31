2a9d0254d025564d0d45aecd7c55f9eabbeb23da
Quailty Ventures <47635254+qualityventures@users.noreply.github.com>
2019-07-31 14:33:37 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,16 +22,6 @@
                     Screenshots come as a file path or as a base64 encoded string. <b>Keep in mind that screenshots are deleted from our server after 1 hour.</b>
                 </p>
             </div>
-            <div class="col-sm-4">
-                <h3>Open Source</h3>
-                <p>The whole platform is Open Source. You can checkout the source code on <a href="https://github.com/stefanzweifel/screeenly">Github</a>.</p>
-            </div>
-            <div class="col-sm-4">
-                <h3>Free</h3>
-                <p>
-                    There are no paid plans or other fees. screeenly currently runs on a 15$ server. If you want to support me you can <a href="https://buymeacoff.ee/3oQ64YW" target="_blank" ref="noopener">buy me a coffee</a> or use <a href="https://m.do.co/c/b8270043ffd3">this link</a> to create your next DigitalOcean Account.
-                </p>
-            </div>
         </div>
 
     </section>
